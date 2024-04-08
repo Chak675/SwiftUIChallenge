d55678f70f36f1aa2a580aa0490f1fffa2697b6b
Dmitrii Zorin <zchakiz@gmail.com>
2024-04-08 09:56:19 +0300

    Initial Commit

---
+++ SwiftUIChallenge/SwiftUIChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIChallengeApp.swift
+//  SwiftUIChallenge
+//
+//  Created by Zorin Dmitrii on 08.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
